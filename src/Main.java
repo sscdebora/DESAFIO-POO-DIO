7dc3c43b51299f1ee2bf589c867d4e22e8691bf1
Debora Cristina <deboracristinasantos5@gmail.com>
2024-06-09 20:55:01 -0300

    implementação de codigo das classes Mentoria, Curso e Main.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Testando");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("desafio poo");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("desafio poo de js");
+        curso2.setCargaHoraria(7);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de java");
+        mentoria1.setDescricao("vai ter desafio de poo");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
